16d463781a0587e68da43e870beda0279ee3858d
ginilmathew3 <ginilmathew3@gmail.com>
2024-07-18 14:13:40 +0530

    change the position of a content in navbar

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -12,11 +12,12 @@ const Navbar = () => {
             <li >
               <a href="#home">Home</a>
             </li>
-            <li>
-              <a href="#skills">Skills</a>
-            </li>
+
             <li>
               <a href="#about">About</a>
+            </li>
+            <li>
+              <a href="#skills">Skills</a>
             </li>
             <li>
               <a href="#contact">Contact</a>
